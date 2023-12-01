248097e918b4beca7c94edb10abe3996a8f0b682
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-12-01 20:25:39 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        element: "#02897A",
+        blue: "#4D8DFF",
+        purple: "#740A76",
+        primary: "#22343D",
+        orange: "#F03E3D",
+      },
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
