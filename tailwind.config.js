1429c213eaa75efb2306647d2fa60a7798483e7f
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-12-01 21:00:23 +0500

    Config done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: [],
+  content: ["./*.{html,js}", "./pages/*.{html,js}"],
   theme: {
     extend: {
       colors: {
